35b53e6b48cb9e3971575839023783e12a7db43f
Yash Patel <mailforyash04@gmail.com>
2022-10-18 00:26:58 +0530

    status code in django

--- blog_app/views.py
+++ blog_app/views.py
@@ -2,6 +2,7 @@ from .models import Blog
 from .serializers import BlogSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
 
 # Create your views here.
 
@@ -10,33 +11,33 @@ def blog_list(request):
     if request.method == "GET":
         all_blogs = Blog.objects.filter(is_public=True)
         serializer = BlogSerializer(all_blogs, many=True)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     if request.method == "POST":
         serializer = BlogSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         else:
-            return Response(serializer.errors)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def blog_detail(request, pk):
     if request.method == "GET":
         blog = Blog.objects.get(pk=pk)
         serializer = BlogSerializer(blog)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     
     if request.method == "PUT":
         blog = Blog.objects.get(pk=pk)
         serializer = BlogSerializer(blog, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         else:
-            return Response(serializer.errors)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
     if request.method == "DELETE":
         blog = Blog.objects.get(pk=pk)
         blog.delete()
-        return Response()
+        return Response(status=status.HTTP_200_OK)
