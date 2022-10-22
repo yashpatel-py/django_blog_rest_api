3a944d96e61dc688dd933cfb52470eab10149449
Yash Patel <mailforyash04@gmail.com>
2022-10-22 15:34:12 +0530

    ModelSerializer

--- blog_app/serializers.py
+++ blog_app/serializers.py
@@ -1,22 +1,30 @@
 from rest_framework import serializers
 from blog_app.models import Blog
 
-class BlogSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField()
-    description = serializers.CharField()
-    post_date = serializers.DateTimeField()
-    is_public = serializers.BooleanField()
-    slug = serializers.CharField()
+class BlogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Blog
+        # fields = "__all__"
+        # fields = ['name', 'description', 'is_public', 'slug']
+        exclude = ['slug']
+
+# ---------------------------- Simple Serializer
+# class BlogSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
+#     name = serializers.CharField()
+#     description = serializers.CharField()
+#     post_date = serializers.DateTimeField()
+#     is_public = serializers.BooleanField()
+#     slug = serializers.CharField()
     
-    def create(self, validated_data):
-        return Blog.objects.create(**validated_data)
+#     def create(self, validated_data):
+#         return Blog.objects.create(**validated_data)
     
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.description = validated_data.get('description', instance.description)
-        instance.post_date = validated_data.get('post_date', instance.post_date)
-        instance.is_public = validated_data.get('is_public', instance.is_public)
-        instance.slug = validated_data.get('slug', instance.slug)
-        instance.save()
-        return instance
+#     def update(self, instance, validated_data):
+#         instance.name = validated_data.get('name', instance.name)
+#         instance.description = validated_data.get('description', instance.description)
+#         instance.post_date = validated_data.get('post_date', instance.post_date)
+#         instance.is_public = validated_data.get('is_public', instance.is_public)
+#         instance.slug = validated_data.get('slug', instance.slug)
+#         instance.save()
+#         return instance
--- blog_app/views.py
+++ blog_app/views.py
@@ -17,7 +17,7 @@ def blog_list(request):
         serializer = BlogSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
