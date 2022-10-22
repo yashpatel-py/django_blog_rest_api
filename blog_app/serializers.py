73130bca93814206ad4c663587a0eff38406a083
Yash Patel <mailforyash04@gmail.com>
2022-10-22 23:43:46 +0530

    Validation

--- blog_app/serializers.py
+++ blog_app/serializers.py
@@ -1,30 +1,57 @@
 from rest_framework import serializers
 from blog_app.models import Blog
 
-# class BlogSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Blog
-#         fields = "__all__"
-        # fields = ['name', 'description', 'is_public', 'slug']
-        # exclude = ['slug']
+def name_valid(value):
+    if len(value) < 4:
+        raise serializers.ValidationError("Blog title is very short")
+    else:
+        return value
 
-# ---------------------------- Simple Serializer
-class BlogSerializer(serializers.Serializer):
+class BlogSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField()
+    name = serializers.CharField(validators = [name_valid])
     description = serializers.CharField()
     post_date = serializers.DateTimeField(required=False)
     is_public = serializers.BooleanField()
     slug = serializers.CharField(required=False)
+
+    class Meta:
+        model = Blog
+        fields = "__all__"
+        # fields = ['name', 'description', 'is_public', 'slug']
+        # exclude = ['slug']
     
-    def create(self, validated_data):
-        return Blog.objects.create(**validated_data)
+    # Field-level validation
+    # def validate_name(self, value):
+    #     if len(value) < 4:
+    #         raise serializers.ValidationError("Blog title is very short")
+    #     else:
+    #         return value
+        
+    # Object-level validation
+    def validate(self, data):
+        if data['name'] == data['description']:
+            raise serializers.ValidationError("Blog title and description can not be same")
+        else:
+            return data
+
+# ---------------------------- Simple Serializer
+# class BlogSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
+#     name = serializers.CharField()
+#     description = serializers.CharField()
+#     post_date = serializers.DateTimeField(required=False)
+#     is_public = serializers.BooleanField()
+#     slug = serializers.CharField(required=False)
     
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.description = validated_data.get('description', instance.description)
-        instance.post_date = validated_data.get('post_date', instance.post_date)
-        instance.is_public = validated_data.get('is_public', instance.is_public)
-        instance.slug = validated_data.get('slug', instance.slug)
-        instance.save()
-        return instance
+#     def create(self, validated_data):
+#         return Blog.objects.create(**validated_data)
+    
+#     def update(self, instance, validated_data):
+#         instance.name = validated_data.get('name', instance.name)
+#         instance.description = validated_data.get('description', instance.description)
+#         instance.post_date = validated_data.get('post_date', instance.post_date)
+#         instance.is_public = validated_data.get('is_public', instance.is_public)
+#         instance.slug = validated_data.get('slug', instance.slug)
+#         instance.save()
+#         return instance
