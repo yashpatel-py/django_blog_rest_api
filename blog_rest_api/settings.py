460857ae7fd039720cd9e8e41864edd254ee679a
yashpatel-py <mailforyash04@gmail.com>
2023-03-10 22:41:29 +0530

    Basic Auth

--- blog_rest_api/settings.py
+++ blog_rest_api/settings.py
@@ -125,8 +125,11 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# REST_FRAMEWORK = {
-#     'DEFAULT_PERMISSION_CLASSES': [
-#         'rest_framework.permissions.IsAuthenticated',
-#     ]
-# }
+REST_FRAMEWORK = {
+    # 'DEFAULT_PERMISSION_CLASSES': [
+    #     'rest_framework.permissions.IsAuthenticated',
+    # ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+    ]
+}
--- blog_rest_api/urls.py
+++ blog_rest_api/urls.py
@@ -19,5 +19,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/blog/', include('blog_app.urls')),
-    path('api-auth/', include('rest_framework.urls'))
 ]
