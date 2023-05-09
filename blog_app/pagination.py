c02f05b55a7f69d57719cdfe5a897917fcf9eab6
yashpatel-py <mailforyash04@gmail.com>
2023-05-09 18:29:14 +0530

    LimitOffsetPagination

--- blog_app/pagination.py
+++ blog_app/pagination.py
@@ -1,7 +1,13 @@
-from rest_framework.pagination import PageNumberPagination
+from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination
 
-class BlogListCreatePagination(PageNumberPagination):
-    page_size = 1
-    # page_query_param = 'p'
-    page_size_query_param = "size"
-    max_page_size = 50
+# class BlogListCreatePagination(PageNumberPagination):
+#     page_size = 1
+#     # page_query_param = 'p'
+#     page_size_query_param = "size"
+#     max_page_size = 50
+
+class BlogListCreatePagination(LimitOffsetPagination):
+    default_limit = 2
+    max_limit = 10
+    limit_query_param = "limit"
+    offset_query_param = "start"
